782fb760dfe50cbbcb63cb1a67547babecb54f6e
Max <maximilian.paulsen@gmail.com>
2017-12-07 20:58:22 -0800

    Added basic structure for path following in Drive subsystem.

--- src/org/ljrobotics/frc2018/Constants.java
+++ src/org/ljrobotics/frc2018/Constants.java
@@ -18,6 +18,25 @@ public class Constants extends ConstantsBase{
 	public static double TRACK_SCRUB_FACTOR = 0.924; //Corrective factor for skidding
 
 	public static double TRACK_WIDTH_INCHES = 20;
+
+	public static double kMinLookAhead;
+	public static double kMaxLookAhead;
+	public static double kMinLookAheadSpeed;
+	public static double kMaxLookAheadSpeed;
+	public static double kInertiaSteeringGain;
+	public static double kPathFollowingProfileKp;
+	public static double kPathFollowingProfileKi;
+	public static double kPathFollowingProfileKv;
+	public static double kPathFollowingProfileKffv;
+	public static double kPathFollowingProfileKffa;
+	public static double kPathFollowingMaxVel;
+	public static double kPathFollowingMaxAccel;
+	public static double kPathFollowingGoalPosTolerance;
+	public static double kPathFollowingGoalVelTolerance;
+	public static double kPathStopSteeringDistance;
+	public static double kDriveHighGearMaxSetpoint;
+	public static double kDriveWheelDiameterInches;
+
 	
 	@Override
 	public String getFileLocation() {
--- src/org/ljrobotics/frc2018/subsystems/Drive.java
+++ src/org/ljrobotics/frc2018/subsystems/Drive.java
@@ -2,7 +2,10 @@ package org.ljrobotics.frc2018.subsystems;
 
 import org.ljrobotics.frc2018.Constants;
 import org.ljrobotics.frc2018.RobotMap;
+import org.ljrobotics.frc2018.RobotState;
 import org.ljrobotics.frc2018.loops.Looper;
+import org.ljrobotics.lib.util.control.Lookahead;
+import org.ljrobotics.lib.util.control.Path;
 import org.ljrobotics.lib.util.control.PathFollower;
 import org.ljrobotics.lib.util.drivers.CANTalonFactory;
 import org.ljrobotics.lib.util.drivers.LazyCANTalon;
@@ -11,13 +14,13 @@ import com.ctre.CANTalon;
 import com.ctre.CANTalon.StatusFrameRate;
 
 /**
- * The Drive subsystem. This subsystem is responsible for everything
- * regarding driving. It controls the four drive motors.
+ * The Drive subsystem. This subsystem is responsible for everything regarding
+ * driving. It controls the four drive motors.
  * 
  * @author Max
  *
  */
-public class Drive extends Subsystem{
+public class Drive extends Subsystem {
 
 	private static Drive instance;
 
@@ -32,37 +35,43 @@ public class Drive extends Subsystem{
 		}
 		return instance;
 	}
-	
+
 	// The robot drivetrain's various states.
 	public enum DriveControlState {
-		VELOCITY_SETPOINT, //Under PID velocity control
-		PATH_FOLLOWING //Following a path
+		VELOCITY_SETPOINT, // Under PID velocity control
+		PATH_FOLLOWING // Following a path
 	}
-	
+
 	public static final int VELOCITY_CONTROL_SLOT = 0;
-	
+
 	// Talons
 	private CANTalon leftMaster;
 	private CANTalon rightMaster;
 	private CANTalon leftSlave;
 	private CANTalon rightSlave;
-	
+
 	// Control States
 	private DriveControlState driveControlState;
-	
+
 	// Controllers
 	private PathFollower pathFollower;
-	
+
 	// Hardware States
 	private boolean isBrakeMode;
+
+	private Path currentPath;
 
 	/**
 	 * Creates a new Drive Subsystem from that controls the given motor controllers.
 	 * 
-	 * @param frontLeft the font left talon motor controller
-	 * @param frontRight the font right talon motor controller
-	 * @param backLeft the back left talon motor controller
-	 * @param backRight the back right talon motor controller
+	 * @param frontLeft
+	 *            the font left talon motor controller
+	 * @param frontRight
+	 *            the font right talon motor controller
+	 * @param backLeft
+	 *            the back left talon motor controller
+	 * @param backRight
+	 *            the back right talon motor controller
 	 */
 	public Drive(CANTalon frontLeft, CANTalon frontRight, CANTalon backLeft, CANTalon backRight) {
 
@@ -70,27 +79,26 @@ public class Drive extends Subsystem{
 		this.rightMaster = frontRight;
 		this.leftSlave = backLeft;
 		this.rightSlave = backRight;
-		
+
 		CANTalonFactory.updateCANTalonToDefault(this.leftMaster);
-        leftMaster.changeControlMode(CANTalon.TalonControlMode.PercentVbus);
-        
-        CANTalonFactory.updatePermanentSlaveTalon(this.leftSlave, this.leftMaster.getDeviceID());
-        leftSlave.reverseOutput(false);
-        leftMaster.setStatusFrameRateMs(StatusFrameRate.Feedback, 5);
-        
+		leftMaster.changeControlMode(CANTalon.TalonControlMode.PercentVbus);
+
+		CANTalonFactory.updatePermanentSlaveTalon(this.leftSlave, this.leftMaster.getDeviceID());
+		leftSlave.reverseOutput(false);
+		leftMaster.setStatusFrameRateMs(StatusFrameRate.Feedback, 5);
+
 		CANTalonFactory.updateCANTalonToDefault(this.rightMaster);
-        rightMaster.changeControlMode(CANTalon.TalonControlMode.PercentVbus);
-        
-        CANTalonFactory.updatePermanentSlaveTalon(this.rightSlave, this.rightMaster.getDeviceID());
-        rightSlave.reverseOutput(false);
-        rightMaster.setStatusFrameRateMs(StatusFrameRate.Feedback, 5);
-		
+		rightMaster.changeControlMode(CANTalon.TalonControlMode.PercentVbus);
+
+		CANTalonFactory.updatePermanentSlaveTalon(this.rightSlave, this.rightMaster.getDeviceID());
+		rightSlave.reverseOutput(false);
+		rightMaster.setStatusFrameRateMs(StatusFrameRate.Feedback, 5);
+
 		this.driveControlState = DriveControlState.VELOCITY_SETPOINT;
 
 		this.isBrakeMode = false;
 		this.setBrakeMode(true);
 	}
-	
 
 	@Override
 	public void stop() {
@@ -100,54 +108,118 @@ public class Drive extends Subsystem{
 
 	@Override
 	public void reset() {
-		
+
 	}
 
 	@Override
 	public void registerEnabledLoops(Looper enabledLooper) {
-		
+
 	}
-	
-	
-	
+
 	/**
-     * Configures talons for velocity control
-     */
-    private void configureTalonsForSpeedControl() {
-        if (!usesTalonVelocityControl(driveControlState)) {
-            // We entered a velocity control state.
-            leftMaster.changeControlMode(CANTalon.TalonControlMode.Speed);
-            leftMaster.setNominalClosedLoopVoltage(12.0);
-            leftMaster.setProfile(VELOCITY_CONTROL_SLOT);
-            leftMaster.configNominalOutputVoltage(Constants.DRIVE_HIGH_GEAR_NOMINAL_OUTPUT,
-                    -Constants.DRIVE_HIGH_GEAR_NOMINAL_OUTPUT);
-            rightMaster.changeControlMode(CANTalon.TalonControlMode.Speed);
-            rightMaster.setNominalClosedLoopVoltage(12.0);
-            rightMaster.setProfile(VELOCITY_CONTROL_SLOT);
-            rightMaster.configNominalOutputVoltage(Constants.DRIVE_HIGH_GEAR_NOMINAL_OUTPUT,
-                    -Constants.DRIVE_HIGH_GEAR_NOMINAL_OUTPUT);
-            setBrakeMode(true);
-        }
-    }
-    
-    /**
-     * Check if the drive talons are configured for velocity control
-     */
-    protected static boolean usesTalonVelocityControl(DriveControlState state) {
-        if (state == DriveControlState.VELOCITY_SETPOINT || state == DriveControlState.PATH_FOLLOWING) {
-            return true;
-        }
-        return false;
+	 * Start up velocity mode. This sets the drive train in high gear as well.
+	 * 
+	 * @param left_inches_per_sec
+	 * @param right_inches_per_sec
+	 */
+	public synchronized void setVelocitySetpoint(double left_inches_per_sec, double right_inches_per_sec) {
+		configureTalonsForSpeedControl();
+		driveControlState = DriveControlState.VELOCITY_SETPOINT;
+		updateVelocitySetpoint(left_inches_per_sec, right_inches_per_sec);
+	}
+
+	/**
+	 * Adjust Velocity setpoint (if already in velocity mode)
+	 * 
+	 * @param left_inches_per_sec
+	 * @param right_inches_per_sec
+	 */
+	private synchronized void updateVelocitySetpoint(double left_inches_per_sec, double right_inches_per_sec) {
+		if (usesTalonVelocityControl(driveControlState)) {
+			final double max_desired = Math.max(Math.abs(left_inches_per_sec), Math.abs(right_inches_per_sec));
+			final double scale = max_desired > Constants.kDriveHighGearMaxSetpoint
+					? Constants.kDriveHighGearMaxSetpoint / max_desired
+					: 1.0;
+			leftMaster.set(inchesPerSecondToRpm(left_inches_per_sec * scale));
+			rightMaster.set(inchesPerSecondToRpm(right_inches_per_sec * scale));
+		} else {
+			System.out.println("Hit a bad velocity control state");
+			leftMaster.set(0);
+			rightMaster.set(0);
+		}
+	}
+
+	private static double inchesToRotations(double inches) {
+        return inches / (Constants.kDriveWheelDiameterInches * Math.PI);
     }
 	
+	private static double inchesPerSecondToRpm(double inches_per_second) {
+		return inchesToRotations(inches_per_second) * 60;
+	}
+
+	/**
+	 * Configures the drivebase to drive a path. Used for autonomous driving
+	 * 
+	 * @see Path
+	 */
+	public synchronized void setWantDrivePath(Path path, boolean reversed) {
+		if (currentPath != path || driveControlState != DriveControlState.PATH_FOLLOWING) {
+			configureTalonsForSpeedControl();
+			RobotState.getInstance().resetDistanceDriven();
+			pathFollower = new PathFollower(path, reversed, new PathFollower.Parameters(
+					new Lookahead(Constants.kMinLookAhead, Constants.kMaxLookAhead, Constants.kMinLookAheadSpeed,
+							Constants.kMaxLookAheadSpeed),
+					Constants.kInertiaSteeringGain, Constants.kPathFollowingProfileKp,
+					Constants.kPathFollowingProfileKi, Constants.kPathFollowingProfileKv,
+					Constants.kPathFollowingProfileKffv, Constants.kPathFollowingProfileKffa,
+					Constants.kPathFollowingMaxVel, Constants.kPathFollowingMaxAccel,
+					Constants.kPathFollowingGoalPosTolerance, Constants.kPathFollowingGoalVelTolerance,
+					Constants.kPathStopSteeringDistance));
+			driveControlState = DriveControlState.PATH_FOLLOWING;
+			currentPath = path;
+		} else {
+			setVelocitySetpoint(0, 0);
+		}
+	}
+
+	/**
+	 * Configures talons for velocity control
+	 */
+	private void configureTalonsForSpeedControl() {
+		if (!usesTalonVelocityControl(driveControlState)) {
+			// We entered a velocity control state.
+			leftMaster.changeControlMode(CANTalon.TalonControlMode.Speed);
+			leftMaster.setNominalClosedLoopVoltage(12.0);
+			leftMaster.setProfile(VELOCITY_CONTROL_SLOT);
+			leftMaster.configNominalOutputVoltage(Constants.DRIVE_HIGH_GEAR_NOMINAL_OUTPUT,
+					-Constants.DRIVE_HIGH_GEAR_NOMINAL_OUTPUT);
+			rightMaster.changeControlMode(CANTalon.TalonControlMode.Speed);
+			rightMaster.setNominalClosedLoopVoltage(12.0);
+			rightMaster.setProfile(VELOCITY_CONTROL_SLOT);
+			rightMaster.configNominalOutputVoltage(Constants.DRIVE_HIGH_GEAR_NOMINAL_OUTPUT,
+					-Constants.DRIVE_HIGH_GEAR_NOMINAL_OUTPUT);
+			setBrakeMode(true);
+		}
+	}
+
+	/**
+	 * Check if the drive talons are configured for velocity control
+	 */
+	protected static boolean usesTalonVelocityControl(DriveControlState state) {
+		if (state == DriveControlState.VELOCITY_SETPOINT || state == DriveControlState.PATH_FOLLOWING) {
+			return true;
+		}
+		return false;
+	}
+
 	public synchronized void setBrakeMode(boolean on) {
 		if (isBrakeMode != on) {
-            isBrakeMode = on;
-            rightMaster.enableBrakeMode(on);
-            rightSlave.enableBrakeMode(on);
-            leftMaster.enableBrakeMode(on);
-            leftSlave.enableBrakeMode(on);
-        }
-    }
+			isBrakeMode = on;
+			rightMaster.enableBrakeMode(on);
+			rightSlave.enableBrakeMode(on);
+			leftMaster.enableBrakeMode(on);
+			leftSlave.enableBrakeMode(on);
+		}
+	}
 
 }
