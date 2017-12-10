b2738ae916b6a940551023f387365005d76cff40
Max <maximilian.paulsen@gmail.com>
2017-12-09 17:31:52 -0800

    Removed double up tabs

--- src/org/ljrobotics/frc2018/utils/Motion.java
+++ src/org/ljrobotics/frc2018/utils/Motion.java
@@ -1,17 +1,19 @@
 package org.ljrobotics.frc2018.utils;
 
-public class Motion{
-private double y;
-private double rotation;
+public class Motion {
+	private double y;
+	private double rotation;
 
-  public Motion(double y, double rotation){
-    this.y=y;
-    this.rotation=rotation;
-  }
-  public double getY(){
-    return this.y;
-  }
-  public double getRotation(){
-    return this.rotation;
-  }
+	public Motion(double y, double rotation) {
+		this.y = y;
+		this.rotation = rotation;
+	}
+
+	public double getY() {
+		return this.y;
+	}
+
+	public double getRotation() {
+		return this.rotation;
+	}
 }
--- test/org/ljrobotics/frc2018/subsystems/DriveTest.java
+++ test/org/ljrobotics/frc2018/subsystems/DriveTest.java
@@ -31,158 +31,167 @@ import com.ctre.CANTalon;
 
 public class DriveTest {
 
-		private Drive drive;
-		private CANTalon frontLeft;
-		private CANTalon frontRight;
-		private CANTalon backLeft;
-		private CANTalon backRight;
+	private Drive drive;
+	private CANTalon frontLeft;
+	private CANTalon frontRight;
+	private CANTalon backLeft;
+	private CANTalon backRight;
 
-		private RobotState robotState;
+	private RobotState robotState;
 
-		@Before
-		public void before() {
-				frontLeft = mock(CANTalon.class);
-				frontRight = mock(CANTalon.class);
-				backLeft = mock(CANTalon.class);
-				backRight = mock(CANTalon.class);
+	@Before
+	public void before() {
+		frontLeft = mock(CANTalon.class);
+		frontRight = mock(CANTalon.class);
+		backLeft = mock(CANTalon.class);
+		backRight = mock(CANTalon.class);
 
-				robotState = mock(RobotState.class);
+		robotState = mock(RobotState.class);
 
-				drive = new Drive(frontLeft, frontRight, backLeft, backRight, robotState);
-		}
+		drive = new Drive(frontLeft, frontRight, backLeft, backRight, robotState);
+	}
 
-		@Test
-		public void stopSetsTalonsToZero() {
-				drive.stop();
-				verifyTalons(0,0,0,0);
-		}
-		@Test
-		public void setToNumTest(){
-			drive.setToNum(0.1);
-			final ArgumentCaptor<Double> captor = ArgumentCaptor.forClass(Double.class);
-			verify(this.frontLeft).set(captor.capture());
-			assertEquals(0.1, (double)captor.getValue(), 0);
-			// verifyTalons(0.1,0.1,0.1,0.1);
+	@Test
+	public void stopSetsTalonsToZero() {
+		drive.stop();
+		verifyTalons(0, 0, 0, 0);
+	}
 
-		}
-		@Test
-		public void moveWithPotitiveY() {
-				drive.move(new Motion(1,0));
-				verifyTalons(1,1,0,0);
-		}
-		@Test
-		public void moveWithNegativeY() {
-				drive.move(new Motion(-1,0));
-				verifyTalons(-1,-1,0,0);
-		}
-		@Test
-		public void moveWithPotitiveRotation() {
-				drive.move(new Motion(0,1));
-				verifyTalons(1,-1,0,0);
-		}
-		@Test
-		public void moveWithNegativeRotation() {
-				drive.move(new Motion(0,-1));
-				verifyTalons(-1,1,0,0);
-		}
-		@Test
-		public void moveWithPositiveYAndRotation() {
-				drive.move(new Motion(0.5,0.5));
-				verifyTalons(1,0,0,0);
-		}
-		@Test
-		public void moveWithNegativeYAndRotation() {
-				drive.move(new Motion(-0.5,-0.5));
-				verifyTalons(-1,0,0,0);
-		}
-		@Test
-		public void moveWithValuesOverOne() {
-				drive.move(new Motion(10,5));
-				verifyTalons(1,0.333333,0,0);
-		}
-		@Test
-		public void moveWithValuesUnderOne() {
-				drive.move(new Motion(-10,-5));
-				verifyTalons(-1,-0.333333,0,0);
-		}
+	@Test
+	public void setToNumTest() {
+		drive.setToNum(0.1);
+		final ArgumentCaptor<Double> captor = ArgumentCaptor.forClass(Double.class);
+		verify(this.frontLeft).set(captor.capture());
+		assertEquals(0.1, (double) captor.getValue(), 0);
+		// verifyTalons(0.1,0.1,0.1,0.1);
 
-		@Test
-		public void setBreakModeSetsBreakModeOnFirstCall() {
-				drive.setBrakeMode(false);
-				drive.setBrakeMode(false);
-				verify(this.frontLeft, times(2)).enableBrakeMode(false);
-				verify(this.frontRight, times(2)).enableBrakeMode(false);
-				verify(this.backLeft, times(2)).enableBrakeMode(false);
-				verify(this.backRight, times(2)).enableBrakeMode(false);
-		}
+	}
 
-		@Test
-		public void setBreakModeSetsBreakModeAfterToggle() {
-				drive.setBrakeMode(false);
-				drive.setBrakeMode(true);
-				verify(this.frontLeft, times(2)).enableBrakeMode(true);
-				verify(this.frontRight, times(2)).enableBrakeMode(true);
-				verify(this.backLeft, times(2)).enableBrakeMode(true);
-				verify(this.backRight, times(2)).enableBrakeMode(true);
-		}
+	@Test
+	public void moveWithPotitiveY() {
+		drive.move(new Motion(1, 0));
+		verifyTalons(1, 1, 0, 0);
+	}
 
-		@Test
-		public void newPathIsNotFinished() {
-				ArrayList<Waypoint> waypoints = new ArrayList<Waypoint>();
-				waypoints.add(new Waypoint(0,0,0,0));
-				waypoints.add(new Waypoint(100,0,0,60));
-				Path path = PathBuilder.buildPathFromWaypoints(waypoints);
-				drive.setWantDrivePath(path, false);
-				assertFalse(drive.isDoneWithPath());
-		}
+	@Test
+	public void moveWithNegativeY() {
+		drive.move(new Motion(-1, 0));
+		verifyTalons(-1, -1, 0, 0);
+	}
 
-		@Test
-		public void isFinishedReturnsTrueAfterPathFinished() {
-				ArrayList<Waypoint> waypoints = new ArrayList<Waypoint>();
-				waypoints.add(new Waypoint(0,0,0,0));
-				waypoints.add(new Waypoint(100,0,0,60));
-				Path path = PathBuilder.buildPathFromWaypoints(waypoints);
-				drive.setWantDrivePath(path, false);
+	@Test
+	public void moveWithPotitiveRotation() {
+		drive.move(new Motion(0, 1));
+		verifyTalons(1, -1, 0, 0);
+	}
 
-				InterpolatingDouble time = new InterpolatingDouble(90D);
-				RigidTransform2d pos = new RigidTransform2d(new Translation2d(0, 1), Rotation2d.fromDegrees(0));
-				Entry<InterpolatingDouble, RigidTransform2d> zeroState =
-						new AbstractMap.SimpleEntry<InterpolatingDouble, RigidTransform2d>(time, pos);
-				when(robotState.getLatestFieldToVehicle()).thenAnswer(i->zeroState);
-				when(robotState.getDistanceDriven()).thenAnswer(i->0D);
+	@Test
+	public void moveWithNegativeRotation() {
+		drive.move(new Motion(0, -1));
+		verifyTalons(-1, 1, 0, 0);
+	}
 
-				Twist2d velocityZero = new Twist2d(0,0,0);
-				when(robotState.getPredictedVelocity()).thenAnswer(i->velocityZero);
+	@Test
+	public void moveWithPositiveYAndRotation() {
+		drive.move(new Motion(0.5, 0.5));
+		verifyTalons(1, 0, 0, 0);
+	}
 
-				drive.updatePathFollower(0);
+	@Test
+	public void moveWithNegativeYAndRotation() {
+		drive.move(new Motion(-0.5, -0.5));
+		verifyTalons(-1, 0, 0, 0);
+	}
 
-				time = new InterpolatingDouble(3D);
-				pos = new RigidTransform2d(new Translation2d(99.9999, 0.0001), Rotation2d.fromDegrees(0));
-				Entry<InterpolatingDouble, RigidTransform2d> entry2 =
-						new AbstractMap.SimpleEntry<InterpolatingDouble, RigidTransform2d>(time, pos);
-				when(robotState.getLatestFieldToVehicle()).thenAnswer(i->entry2);
-				when(robotState.getDistanceDriven()).thenAnswer(i->99.9999D);
+	@Test
+	public void moveWithValuesOverOne() {
+		drive.move(new Motion(10, 5));
+		verifyTalons(1, 0.333333, 0, 0);
+	}
 
-				Twist2d velocity2 = new Twist2d(0,0,0);
-				when(robotState.getPredictedVelocity()).thenAnswer(i->velocity2);
+	@Test
+	public void moveWithValuesUnderOne() {
+		drive.move(new Motion(-10, -5));
+		verifyTalons(-1, -0.333333, 0, 0);
+	}
 
-				drive.updatePathFollower(3);
-				assertTrue(drive.isDoneWithPath());
-		}
+	@Test
+	public void setBreakModeSetsBreakModeOnFirstCall() {
+		drive.setBrakeMode(false);
+		drive.setBrakeMode(false);
+		verify(this.frontLeft, times(2)).enableBrakeMode(false);
+		verify(this.frontRight, times(2)).enableBrakeMode(false);
+		verify(this.backLeft, times(2)).enableBrakeMode(false);
+		verify(this.backRight, times(2)).enableBrakeMode(false);
+	}
 
-		private void verifyTalons(double frontLeft, double frontRight, double backLeft, double backRight) {
-				final ArgumentCaptor<Double> captor = ArgumentCaptor.forClass(Double.class);
-				verify(this.frontLeft).set(captor.capture());
-				assertEquals(frontLeft, (double)captor.getValue(), 0.00001);
+	@Test
+	public void setBreakModeSetsBreakModeAfterToggle() {
+		drive.setBrakeMode(false);
+		drive.setBrakeMode(true);
+		verify(this.frontLeft, times(2)).enableBrakeMode(true);
+		verify(this.frontRight, times(2)).enableBrakeMode(true);
+		verify(this.backLeft, times(2)).enableBrakeMode(true);
+		verify(this.backRight, times(2)).enableBrakeMode(true);
+	}
 
-				verify(this.frontRight).set(captor.capture());
-				assertEquals(frontRight, (double)captor.getValue(), 0.00001);
+	@Test
+	public void newPathIsNotFinished() {
+		ArrayList<Waypoint> waypoints = new ArrayList<Waypoint>();
+		waypoints.add(new Waypoint(0, 0, 0, 0));
+		waypoints.add(new Waypoint(100, 0, 0, 60));
+		Path path = PathBuilder.buildPathFromWaypoints(waypoints);
+		drive.setWantDrivePath(path, false);
+		assertFalse(drive.isDoneWithPath());
+	}
 
-				verify(this.backLeft).set(captor.capture());
-				assertEquals(backLeft, (double)captor.getValue(), 0.00001);
+	@Test
+	public void isFinishedReturnsTrueAfterPathFinished() {
+		ArrayList<Waypoint> waypoints = new ArrayList<Waypoint>();
+		waypoints.add(new Waypoint(0, 0, 0, 0));
+		waypoints.add(new Waypoint(100, 0, 0, 60));
+		Path path = PathBuilder.buildPathFromWaypoints(waypoints);
+		drive.setWantDrivePath(path, false);
 
-				verify(this.backRight).set(captor.capture());
-				assertEquals(backRight, (double)captor.getValue(), 0.00001);
-		}
+		InterpolatingDouble time = new InterpolatingDouble(90D);
+		RigidTransform2d pos = new RigidTransform2d(new Translation2d(0, 1), Rotation2d.fromDegrees(0));
+		Entry<InterpolatingDouble, RigidTransform2d> zeroState = new AbstractMap.SimpleEntry<InterpolatingDouble, RigidTransform2d>(
+				time, pos);
+		when(robotState.getLatestFieldToVehicle()).thenAnswer(i -> zeroState);
+		when(robotState.getDistanceDriven()).thenAnswer(i -> 0D);
+
+		Twist2d velocityZero = new Twist2d(0, 0, 0);
+		when(robotState.getPredictedVelocity()).thenAnswer(i -> velocityZero);
+
+		drive.updatePathFollower(0);
+
+		time = new InterpolatingDouble(3D);
+		pos = new RigidTransform2d(new Translation2d(99.9999, 0.0001), Rotation2d.fromDegrees(0));
+		Entry<InterpolatingDouble, RigidTransform2d> entry2 = new AbstractMap.SimpleEntry<InterpolatingDouble, RigidTransform2d>(
+				time, pos);
+		when(robotState.getLatestFieldToVehicle()).thenAnswer(i -> entry2);
+		when(robotState.getDistanceDriven()).thenAnswer(i -> 99.9999D);
+
+		Twist2d velocity2 = new Twist2d(0, 0, 0);
+		when(robotState.getPredictedVelocity()).thenAnswer(i -> velocity2);
+
+		drive.updatePathFollower(3);
+		assertTrue(drive.isDoneWithPath());
+	}
+
+	private void verifyTalons(double frontLeft, double frontRight, double backLeft, double backRight) {
+		final ArgumentCaptor<Double> captor = ArgumentCaptor.forClass(Double.class);
+		verify(this.frontLeft).set(captor.capture());
+		assertEquals(frontLeft, (double) captor.getValue(), 0.00001);
+
+		verify(this.frontRight).set(captor.capture());
+		assertEquals(frontRight, (double) captor.getValue(), 0.00001);
+
+		verify(this.backLeft).set(captor.capture());
+		assertEquals(backLeft, (double) captor.getValue(), 0.00001);
+
+		verify(this.backRight).set(captor.capture());
+		assertEquals(backRight, (double) captor.getValue(), 0.00001);
+	}
 
 }
