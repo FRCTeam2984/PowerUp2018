754da8e0cb95464c434a82308810c84ea43718b5
Max <maximilian.paulsen@gmail.com>
2017-12-10 20:48:33 -0800

    removed unused import

--- src/org/ljrobotics/frc2018/Robot.java
+++ src/org/ljrobotics/frc2018/Robot.java
@@ -7,10 +7,8 @@ import org.ljrobotics.lib.util.CrashTracker;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.command.Command;
-import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  * The VM is configured to automatically run this class, and to call the
