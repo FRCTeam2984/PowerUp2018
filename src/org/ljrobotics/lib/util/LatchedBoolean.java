8f7ec07f88fc84cd20c1e8e4eff85868e5f16a7c
Max <maximilian.paulsen@gmail.com>
2017-12-17 14:33:51 -0800

    Added a test for latched boolean and updated naming conventions

--- src/org/ljrobotics/lib/util/LatchedBoolean.java
+++ src/org/ljrobotics/lib/util/LatchedBoolean.java
@@ -3,17 +3,18 @@ package org.ljrobotics.lib.util;
 /**
  * An iterative boolean latch.
  * 
- * Returns true once if and only if the value of newValue changes from false to true.
+ * Returns true once if and only if the value of newValue changes from false to
+ * true.
  */
 public class LatchedBoolean {
-    private boolean mLast = false;
+    private boolean last = false;
 
     public boolean update(boolean newValue) {
         boolean ret = false;
-        if (newValue && !mLast) {
+        if (newValue && !last) {
             ret = true;
         }
-        mLast = newValue;
+        last = newValue;
         return ret;
     }
 }
