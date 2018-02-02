5d56c6e93874fd5a684f36294c47eb76a7f01cc3
ruwix <grantpauker@gmail.com>
2018-02-01 19:08:58 -0800

    add more arm constants

--- src/org/ljrobotics/frc2018/Constants.java
+++ src/org/ljrobotics/frc2018/Constants.java
@@ -21,53 +21,53 @@
                                  \:|   |                  \::/    /        
                                   \|___|                   \/____/         
                                                                           
-*/  
+*/
 package org.ljrobotics.frc2018;
 
 import org.ljrobotics.lib.util.ConstantsBase;
 
-public class Constants extends ConstantsBase{
+public class Constants extends ConstantsBase {
 	// Gyroscope configuration
-	
+
 	public static double GYRO_MODIFIER = 1;
 	// Path following configuration
 
-	public static double SEGMENT_COMPLETION_TOLERANCE = 0.1; //inches
+	public static double SEGMENT_COMPLETION_TOLERANCE = 0.1; // inches
 
-	public static double PATH_FOLLOWING_MAX_ACCEL = 120; //inches per second^2
-	public static double PATH_FOLLOWING_MAX_VEL = 120; //inches per second
+	public static double PATH_FOLLOWING_MAX_ACCEL = 120; // inches per second^2
+	public static double PATH_FOLLOWING_MAX_VEL = 120; // inches per second
 
 	public static double PATH_FOLLOWING_GOAL_POS_TOLERANCE = 0.75;
 	public static double PATH_FOLLOWING_GOAL_VEL_TOLERANCE = 12.0;
 	public static double PATH_STOP_STEERING_DISTANCE = 9.0;
 	public static double DRIVE_HIGH_GEAR_NOMINAL_OUTPUT = 0.5;
 
-	public static double TRACK_SCRUB_FACTOR = 0.924; //Corrective factor for skidding
+	public static double TRACK_SCRUB_FACTOR = 0.924; // Corrective factor for skidding
 
 	public static double TRACK_WIDTH_INCHES = 19.75;
 	// To edit for path following
-	
+
 	public static double MIN_LOOK_AHEAD = 12;
 	public static double MAX_LOOK_AHEAD = 24;
 	public static double MIN_LOOK_AHEAD_SPEED = 9;
 	public static double MAX_LOOK_AHEAD_SPEED = 120;
-	
+
 	public static double PATH_FOLLOWING_PROFILE_Kp = 0.5;
 	public static double PATH_FOLLOWING_PROFILE_Ki = 0.003;
 	public static double PATH_FOLLOWING_PROFILE_Kv = 0.02;
 	public static double PATH_FOLLOWING_PROFILE_Kffv = 0.5;
 	public static double PATH_FOLLOWING_PROFILE_Kffa = 0.05;
 	// Do not touch
-	
+
 	public static double INERTIA_STEERING_GAIN = 0;
-	
-	public static double DRIVE_MAX_SETPOINT = 100; //Inches Per Second
+
+	public static double DRIVE_MAX_SETPOINT = 100; // Inches Per Second
 	public static double DRIVE_WHEEL_DIAMETER_INCHES = 6;
 	public static int DRIVE_ENCODER_TICKS_PER_ROTATION_RIGHT = 248;
 	public static int DRIVE_ENCODER_TICKS_PER_ROTATION_LEFT = -1470;
 	// RIGHT FOR 2018: 141.895/36.25*360, Left for 2018: -34.558/36.25*-1440
-	// RIGHT FOR 2017: 32/39*303    , left for 2017: 24.488/25*-1440
-	
+	// RIGHT FOR 2017: 32/39*303 , left for 2017: 24.488/25*-1440
+
 	public static double DRIVE_Kp = 0.00005;
 	public static double DRIVE_Ki = 0.0000001;
 	public static double DRIVE_Kd = 0;
@@ -76,17 +76,17 @@ public class Constants extends ConstantsBase{
 	// Intake
 	public static double SUCK_SPEED = 1;
 	public static double SPIT_SPEED = -1;
-	
-	public static int LEFT_INTAKE_MOTOR_ID = 26;//29
-	public static int RIGHT_INTAKE_MOTOR_ID = 27;//28
+
+	public static int LEFT_INTAKE_MOTOR_ID = 26;// 29
+	public static int RIGHT_INTAKE_MOTOR_ID = 27;// 28
 	public static int TENSION_INTAKE_MOTOR_ID = 20;
-	
+
 	public static int MAX_SUCK_CURRENT = 20;
 	public static int NOMINAL_SUCK_CURRENT = 10;
 	public static int MAX_SUCK_CURRENT_TIME = 1000;
 	// -----------------------------------------------------------------------------------------------
 	// Control Config
-	
+
 	public static int JOYSTICK_ROTATION_AXIS = 2;
 	public static boolean USE_TANK_DRIVE = false;
 	public static int JOYSTICK_POWER_POWER = 3;
@@ -95,7 +95,7 @@ public class Constants extends ConstantsBase{
 	public static int QUICKTURN_BUTTON = 7;
 	// -----------------------------------------------------------------------------------------------
 	// Loopers
-	
+
 	public static double LOOPER_Dt = 0.005;
 	// -----------------------------------------------------------------------------------------------
 	// Android configuration
@@ -104,22 +104,22 @@ public class Constants extends ConstantsBase{
 	// -----------------------------------------------------------------------------------------------
 	// Motor configuration
 
-	public static int JOYSTICK_DRIVE_ID =  0x0;
+	public static int JOYSTICK_DRIVE_ID = 0x0;
 	public static int JOYSTICK_OPERATOR_ID = 1;
-	public static final int FRONT_LEFT_MOTOR_ID =  12;
+	public static final int FRONT_LEFT_MOTOR_ID = 12;
 	public static final int FRONT_RIGHT_MOTOR_ID = 10;
-	public static final int REAR_LEFT_MOTOR_ID =   23;
-	public static final int REAR_RIGHT_MOTOR_ID =  22;
+	public static final int REAR_LEFT_MOTOR_ID = 23;
+	public static final int REAR_RIGHT_MOTOR_ID = 22;
 	// -----------------------------------------------------------------------------------------------
 	// General
 	public static double EQUALS_EPSILON = 0.00001;
 
 	// -----------------------------------------------------------------------------------------------
-	//Camera position and rotation
-	
-	public static double CAMERA_X_OFFSET = 0; //From wheelbase center, looking down
-	public static double CAMERA_Y_OFFSET = 0; //From wheelbase center, looking down
-	public static double CAMERA_YAW = 0; //In degrees, + is anti-clockwise viewed from above
+	// Camera position and rotation
+
+	public static double CAMERA_X_OFFSET = 0; // From wheelbase center, looking down
+	public static double CAMERA_Y_OFFSET = 0; // From wheelbase center, looking down
+	public static double CAMERA_YAW = 0; // In degrees, + is anti-clockwise viewed from above
 
 	public static double MAX_TRACKER_DISTANCE = 18;
 	public static double MAX_GOAL_TRACK_AGE = 1;
@@ -133,12 +133,17 @@ public class Constants extends ConstantsBase{
 
 	public static int MAX_ARM_CURRENT_TIME = 1;
 
-//	public static int SLAVE_ARM_MOTOR_ID = 28;
+	public static int SLAVE_ARM_MOTOR_ID = 28;
 
 	public static int MASTER_ARM_MOTOR_ID = 4;
 
-	 public static final int MIN_ARM_ENCODER_VALUE = 0;
-	 public static final int MAX_ARM_ENCODER_VALUE = 10;
+	public static int MIN_ARM_ENCODER_VALUE = 0;
+	public static int MAX_ARM_ENCODER_VALUE = 10;
+
+	public static double ARM_TICKS_PER_REVOLUTION = 4960;
+	public static double ARM_GEAR_RATIO = 30D/11D;
+	public static double ARM_INVERSE_GEAR_RATIO = 11D/30D;
+
 
 
 	@Override
@@ -147,29 +152,16 @@ public class Constants extends ConstantsBase{
 		return "/home/lvuser/constants";
 	}
 
-	
 	/*
-	{
-        "DRIVE_ENCODER_TICKS_PER_ROTATION_RIGHT":1439,
-        "DRIVE_ENCODER_TICKS_PER_ROTATION_LEFT":1418,
-        "GYRO_MODIFIER":-1,
-        "DRIVE_Kf": 0.00875,
-        "DRIVE_Kp": 0.015,
-        "DRIVE_Ki": 0,
-        "DRIVE_Kd": -0.0003,
-        "PATH_FOLLOWING_GOAL_POS_TOLERANCE": 3,
-        "PATH_FOLLOWING_MAX_ACCEL": 60,
-        "PATH_FOLLOWING_MAX_VEL": 80,
-        "PATH_FOLLOWING_PROFILE_Kffa": 0.05,
-        "MIN_LOOK_AHEAD_SPEED": 9,
-        "PATH_FOLLOWING_PROFILE_Kp": 1.5,
-        "PATH_FOLLOWING_PROFILE_Ki": 0.002,
-        "MIN_LOOK_AHEAD": 12,
-        "PATH_FOLLOWING_PROFILE_Kv": 0.1,
-        "SEGMENT_COMPLETION_TOLERANCE": 4,
-        "JOYSTICK_ROTATION_AXIS": 2,
-        "USE_TANK_DRIVE": false,
-        "QUICKTURN_BUTTON": 5,
-        "JOYSTICK_POWER_POWER": 1
-	}*/
+	 * { "DRIVE_ENCODER_TICKS_PER_ROTATION_RIGHT":1439,
+	 * "DRIVE_ENCODER_TICKS_PER_ROTATION_LEFT":1418, "GYRO_MODIFIER":-1, "DRIVE_Kf":
+	 * 0.00875, "DRIVE_Kp": 0.015, "DRIVE_Ki": 0, "DRIVE_Kd": -0.0003,
+	 * "PATH_FOLLOWING_GOAL_POS_TOLERANCE": 3, "PATH_FOLLOWING_MAX_ACCEL": 60,
+	 * "PATH_FOLLOWING_MAX_VEL": 80, "PATH_FOLLOWING_PROFILE_Kffa": 0.05,
+	 * "MIN_LOOK_AHEAD_SPEED": 9, "PATH_FOLLOWING_PROFILE_Kp": 1.5,
+	 * "PATH_FOLLOWING_PROFILE_Ki": 0.002, "MIN_LOOK_AHEAD": 12,
+	 * "PATH_FOLLOWING_PROFILE_Kv": 0.1, "SEGMENT_COMPLETION_TOLERANCE": 4,
+	 * "JOYSTICK_ROTATION_AXIS": 2, "USE_TANK_DRIVE": false, "QUICKTURN_BUTTON": 5,
+	 * "JOYSTICK_POWER_POWER": 1 }
+	 */
 }
