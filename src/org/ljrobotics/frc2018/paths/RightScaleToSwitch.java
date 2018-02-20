976dc69012e970d9388cb84a24d5e32b8cf2dd85
Max <maximilian.paulsen@gmail.com>
2018-02-20 06:34:43 -0800

    Added to double cube paths

---
+++ src/org/ljrobotics/frc2018/paths/RightScaleToSwitch.java
@@ -0,0 +1,37 @@
+package org.ljrobotics.frc2018.paths;
+
+import java.util.ArrayList;
+
+import org.ljrobotics.lib.util.control.Path;
+import org.ljrobotics.lib.util.control.PathBuilder;
+import org.ljrobotics.lib.util.control.PathBuilder.Waypoint;
+import org.ljrobotics.lib.util.control.PathContainer;
+import org.ljrobotics.lib.util.math.RigidTransform2d;
+import org.ljrobotics.lib.util.math.Rotation2d;
+import org.ljrobotics.lib.util.math.Translation2d;
+
+public class RightScaleToSwitch implements PathContainer {
+
+    @Override
+    public Path buildPath() {
+        ArrayList<Waypoint> sWaypoints = new ArrayList<Waypoint>();
+        sWaypoints.add(new Waypoint(325,58,0,60));
+        sWaypoints.add(new Waypoint(265,58,20,60));
+        sWaypoints.add(new Waypoint(227,76,0,60));
+
+        return PathBuilder.buildPathFromWaypoints(sWaypoints);
+    }
+
+    @Override
+    public RigidTransform2d getStartPose() {
+        return new RigidTransform2d(new Translation2d(325, 58), Rotation2d.fromDegrees(180.0));
+    }
+
+    @Override
+    public boolean isReversed() {
+        return false;
+    }
+	// WAYPOINT_DATA: [{"position":{"x":325,"y":58},"speed":60,"radius":0,"comment":""},{"position":{"x":265,"y":58},"speed":60,"radius":20,"comment":""},{"position":{"x":227,"y":76},"speed":60,"radius":0,"comment":""}]
+	// IS_REVERSED: false
+	// FILE_NAME: UntitledPath
+}
